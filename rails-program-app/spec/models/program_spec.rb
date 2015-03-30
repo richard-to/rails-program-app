3a3f0ca4b191407af16679f89fc31e648b633374
richard-to <gohn76@gmail.com>
2015-03-30 14:58:46 -0700

    Add specs for model validation

--- rails-program-app/spec/models/program_spec.rb
+++ rails-program-app/spec/models/program_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Program, type: :model do
+
+  let(:valid_attributes) {
+    { title: 'title', subtitle: 'subtitle', code: '123456' }
+  }
+
+  let(:invalid_code) {
+    123
+  }
+
   it "can create a program instance" do
     program = Program.new
   end
+
+  it "can create an valid program" do
+    program = Program.new(valid_attributes)
+    expect(program.valid?).to be_truthy
+  end
+
+  context "cannot create a valid program" do
+    it "requires a 6-character code" do
+      program = Program.new(valid_attributes)
+      program.code = invalid_code
+      expect(program.valid?).to be_falsey
+    end
+
+    it "requires a title" do
+      program = Program.new(valid_attributes)
+      program.title = ''
+      expect(program.valid?).to be_falsey
+    end
+
+    it "requires a subtitle" do
+      program = Program.new(valid_attributes)
+      program.subtitle = ''
+      expect(program.valid?).to be_falsey
+    end
+
+  end
+
 end
