9de96467ab68cd1049c083b68903d65b97704a5b
richard-to <gohn76@gmail.com>
2015-03-30 18:00:13 -0700

    Add implementation for model defaults

--- rails-program-app/app/models/language.rb
+++ rails-program-app/app/models/language.rb
@@ -1,5 +1,12 @@
 class Language < ActiveRecord::Base
+  after_initialize :defaults
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
   has_many :programs
+
+  private
+  def defaults
+    self.name ||= ''
+  end
+
 end
