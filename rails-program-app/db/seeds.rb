beaf32986ff093468d599586ab97007a148fc6cb
richard-to <gohn76@gmail.com>
2015-03-30 17:27:01 -0700

    Fix typo in seeds.rb

--- rails-program-app/db/seeds.rb
+++ rails-program-app/db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-languages = ['c', 'c++', 'go', 'haskell', 'java' 'javascript', 'php', 'python', 'ruby', 'rust', 'scala']
+languages = ['c', 'c++', 'go', 'haskell', 'java', 'javascript', 'php', 'python', 'ruby', 'rust', 'scala']
 languages.each do |language|
   Language.create(name: language)
 end
