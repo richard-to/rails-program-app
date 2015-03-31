96d2d33b2328dc3620eec2aaae0529b3ba9fc557
richard-to <gohn76@gmail.com>
2015-03-30 17:14:51 -0700

    Add Spec for language model

--- rails-program-app/spec/models/language_spec.rb
+++ rails-program-app/spec/models/language_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Language, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) {
+    { name: 'language 1' }
+  }
+
+  it "can create a language instance" do
+    language = Language.new
+  end
+
+  it "can create a valid language" do
+    language = Language.new(valid_attributes)
+    expect(language.valid?).to be_truthy
+  end
+
+  it "cannot create a language that already exists" do
+    Language.create!(valid_attributes)
+    language = Language.new(valid_attributes)
+    expect(language.valid?).to be_falsey
+  end
 end
