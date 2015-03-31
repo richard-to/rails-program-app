392180b07e88ea46d89c6badb6730168be7ed732
richard-to <gohn76@gmail.com>
2015-03-30 17:40:27 -0700

    Add test to make sure language not displayed in show view if not exists

--- rails-program-app/spec/views/programs/show.html.erb_spec.rb
+++ rails-program-app/spec/views/programs/show.html.erb_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "programs/show", type: :view do
-  before(:each) do
+
+  it "renders all attributes in <dd>" do
     language = Language.create(:name => 'Test Language')
     @program = assign(:program, Program.create!(
       :title => "Title",
@@ -9,13 +10,25 @@ RSpec.describe "programs/show", type: :view do
       :code => "123456",
       :language => language
     ))
-  end
 
-  it "renders attributes in <p>" do
     render
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/Subtitle/)
     expect(rendered).to match(/Code/)
     expect(rendered).to match(/Test Language/)
+  end
+
+  it "renders required attributes" do
+    @program = assign(:program, Program.create!(
+      :title => "Title",
+      :subtitle => "Subtitle",
+      :code => "123456",
+    ))
+
+    render
+    expect(rendered).to match(/Title/)
+    expect(rendered).to match(/Subtitle/)
+    expect(rendered).to match(/Code/)
+    expect(rendered).not_to match(/Test Language/)
   end
 end
